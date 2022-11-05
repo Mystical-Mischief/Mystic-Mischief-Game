18c4ec83bdd7bcedb6c1e9fdb1ae171b7c8a3fbd
Miryxx <57016232+Miryxx@users.noreply.github.com>
2022-11-05 18:21:56 -0400

    Spider AI pt.1
    Spider AI is almost finished I just need to get the stun to work. Will need bug fixes because things are a little funky but they work.

---
+++ Assets/Scripts/AI/SpiderAI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderAI : BaseEnemyAI
+{
+    ThirdPersonController player;
+    public GameObject projectile;
+
+    float timeBetweenShots;
+    public float startTimeBetweenShots;
+
+    new void Start()
+    {
+        base.Start();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<ThirdPersonController>();
+
+        timeBetweenShots = startTimeBetweenShots;
+    }
+
+    new void Update()
+    {
+        base.Update();
+
+        if (spottedPlayer == true)
+        {
+            if (timeBetweenShots <= 0)
+            {
+                Instantiate(projectile, transform.position, Quaternion.identity);
+                timeBetweenShots = startTimeBetweenShots;
+            }
+            else
+            {
+                timeBetweenShots -= Time.deltaTime;
+            }
+        }
+    }
+}
