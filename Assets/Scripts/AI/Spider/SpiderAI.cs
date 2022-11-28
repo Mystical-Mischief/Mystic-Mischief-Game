33036fa125d7c49e0c4e9781a4595a367d1a5fe3
AGoodShake <progers7@knights.ucf.edu>
2022-11-28 13:57:36 -0500

    push spider animations

--- Assets/Scripts/AI/Spider/SpiderAI.cs
+++ Assets/Scripts/AI/Spider/SpiderAI.cs
@@ -29,6 +29,7 @@ public class SpiderAI : BaseEnemyAI
             if (timeBetweenShots <= 0)
             {
                 //fires projectile at the location the player was located at when the spider first shot. It does not follow player
+                ShootAnim();
                 Instantiate(projectile, transform.position, Quaternion.identity);
                 timeBetweenShots = startTimeBetweenShots;
             }
@@ -37,6 +38,11 @@ public class SpiderAI : BaseEnemyAI
                 timeBetweenShots -= Time.deltaTime;
             }
         }
+    }
+
+    public virtual void ShootAnim()
+    {
+                anim.SetTrigger("Web");
     }
 
     public virtual void FoundPlayer(GameObject player)
