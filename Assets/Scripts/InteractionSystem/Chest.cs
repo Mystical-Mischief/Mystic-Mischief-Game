18a53b946ea6613915ee0389ce806ab8d6b0c53b
Cruzky40g <cruzky40g@gmail.com>
2022-11-28 21:16:53 -0500

    Updated Inventory and Quest System

--- Assets/Scripts/InteractionSystem/Chest.cs
+++ Assets/Scripts/InteractionSystem/Chest.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Chest : MonoBehaviour, IInteractable
 {
-   [SerializeField] private string _prompt;
+    public Quest quest;
+    [SerializeField] private string _prompt;
 
     public string InteractionPrompt => _prompt; 
 
     public bool Interact(Interactor interactor)
     {
         Debug.Log( "opening Chest!");
+        if(quest != null)
+        {
+            quest.UpdateQuest();
+        } 
         return true;
     }
 }
