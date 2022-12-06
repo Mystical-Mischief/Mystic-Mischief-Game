3183dc2a34c3e21b4e9e21e071e702351defd88a
Brendan <weegeegotme@gmail.com>
2022-12-06 17:49:35 -0500

    Improved Lasso visual
    I also added a script for it to work

--- Assets/Models/Hats/Lasso/RopeStretcher.cs
+++ Assets/Models/Hats/Lasso/RopeStretcher.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class RopeStretcher : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //I obtain the two public gameobjects
+    public GameObject StartOfRope;
+    public GameObject LassoProjectile;
+    //the distance between the two gameobjects
+    float distance;
 
-    // Update is called once per frame
+
+   
+
+    
     void Update()
     {
+        //I made the code this way so that we can disable the method if we need to
+        Stretch();
+    }
+    void Stretch()
+    {
+        //I check if the value is null in case the gameobject slots are empty
+        if(distance != null)
+        {
+            //this function gives us the distance between the two gameobjects
+            distance = Vector3.Distance(StartOfRope.transform.position, LassoProjectile.transform.position);
+        }
+        
+        //The script only runs if the distance is greater than 2 to prevent the rope from shrinking
+        if(distance >2)
+        {
+            this.gameObject.transform.localScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y, distance/2);
+        }
         
     }
 }
