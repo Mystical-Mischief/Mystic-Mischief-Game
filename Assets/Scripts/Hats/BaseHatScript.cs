c11058c57c4bc301ab75a3787e166453a3251bad
Cruzky40g <cruzky40g@gmail.com>
2023-02-14 17:39:28 -0500

    Fixed Explorers Hat

--- Assets/Scripts/Hats/BaseHatScript.cs
+++ Assets/Scripts/Hats/BaseHatScript.cs
@@ -33,8 +33,9 @@ public class BaseHatScript : MonoBehaviour
     }
     public void Update()
     {
+        activateHat = controls.Actions.ActivateHat.IsPressed();
         //if you can use the hat use the hat and start the cooldown
-        if (controls.Actions.ActivateHat.IsPressed() && canUseHat)
+        if (activateHat && canUseHat)
         {
             canUseHat = false;
             HatAbility();
