cb17b9fda236e19b40add57f73d3d9e9d2aef29c
EmilieGanmont <77340577+EmilieGanmont@users.noreply.github.com>
2023-02-14 22:59:24 -0500

    pause menu bug fix

--- Assets/Scripts/Menus/PauseMenu.cs
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -60,7 +60,7 @@ public class PauseMenu : MonoBehaviour
     public void LoadMenu()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("Main Menu");
         Debug.Log("Loading menu...");
     }
 
