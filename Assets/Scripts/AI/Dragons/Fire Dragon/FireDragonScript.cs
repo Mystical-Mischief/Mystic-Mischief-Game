e95f5580a73a15dd54e2ebe2bbdf4a45f558c336
AGoodShake <progers7@knights.ucf.edu>
2023-02-24 17:26:59 -0500

    push fire dragon fixes

--- Assets/Scripts/AI/Dragons/Fire Dragon/FireDragonScript.cs
+++ Assets/Scripts/AI/Dragons/Fire Dragon/FireDragonScript.cs
@@ -10,7 +10,7 @@ public class FireDragonScript : BasicfireDragonAI
 {
     // public Transform target;
     public bool SeesPlayer;
-    public LayerMask layerMask;
+    // public LayerMask layerMask;
     private float dist;
     public bool spotsPlayer;
     // public ParticleSystem ps;
@@ -37,54 +37,54 @@ public class FireDragonScript : BasicfireDragonAI
     {
         //This sets the enemy vision range it can detect tot the agression meter.
         ev.viewRadius = agressionMeter;
-        float closeDist = Vector3.Distance(base.target.position, transform.position);
+        // float closeDist = Vector3.Distance(base.target.position, transform.position);
         base.Update();
-        RaycastHit hit;
+        // RaycastHit hit;
         // Does the ray intersect any objects excluding the player layer
         // if (spottedPlayer == true)
         // {
             // transform.LookAt(target);
-            float maxRange = 5;
-        if (Physics.Raycast(transform.position, (base.target.position - transform.position), out hit, maxRange, layerMask))
-        {
-            Debug.DrawRay(transform.position, (base.target.position - transform.position) * maxDistance, Color.yellow);
-            // Debug.Log("Hit Wall");
-            if (hit.transform.tag == "Player")
-            {
-                SeesPlayer = true;
-            }
-            else
-            {
-                SeesPlayer = false;
-            }
-        }
-        else
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-            // Debug.Log("Hit Player");
-        }
+            // float maxRange = 5;
+        // if (Physics.Raycast(transform.position, (base.target.position - transform.position), out hit, maxRange, layerMask))
+        // {
+        //     Debug.DrawRay(transform.position, (base.target.position - transform.position) * maxDistance, Color.yellow);
+        //     // Debug.Log("Hit Wall");
+        //     if (hit.transform.tag == "Player")
+        //     {
+        //         SeesPlayer = true;
+        //     }
+        //     else
+        //     {
+        //         SeesPlayer = false;
+        //     }
+        // }
+        // else
+        // {
+        //     Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+        //     // Debug.Log("Hit Player");
+        // }
         //dist <= attackDist && 
         //If the dragon is in the air and spots the player it shoots fire breath.
         if (base.inAir == true && base.spottedPlayer == true)
         {
             ps.Play(true);
         }
-        // else 
-        // {
-        //     ps.Stop(true);
-        // }
+        else 
+        {
+            ps.Stop(true);
+        }
 
         // If it is close to the dragon it sets the attack to true.
-         if (dist > 2f && dist <= meleeDist)
-         {
-            //transform.LookAt(PlayerPos);
-            if (meleeAttack == false)
-            {
-            attackPos.SetActive(true);
-            meleeAttack = true;
-            attacked = true;
-            }
+        //  if (dist > 2f && dist <= meleeDist)
+        //  {
+        //     //transform.LookAt(PlayerPos);
+        //     if (meleeAttack == false)
+        //     {
+        //     attackPos.SetActive(true);
+        //     meleeAttack = true;
+        //     attacked = true;
+        //     }
             // else {anim.SetBool("Bite 0", false);}
-         }
+        //  }
     }
 }
