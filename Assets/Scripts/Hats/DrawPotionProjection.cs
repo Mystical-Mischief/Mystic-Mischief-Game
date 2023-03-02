1957abfe2354ae39080e7973c49eab8455b9acb3
danieldra910 <daniel_arias2000@knights.ucf.edu>
2023-03-01 22:48:33 -0500

    Trajectory Line for wizard hat

--- Assets/Scripts/Hats/DrawPotionProjection.cs
+++ Assets/Scripts/Hats/DrawPotionProjection.cs
@@ -8,17 +8,22 @@ public class DrawPotionProjection : MonoBehaviour
 {
     private LineRenderer lineRenderer;
 
-    [Range(3f, 100f)]
+    [SerializeField][Range(3f, 100f)]
     private int _lineSegments = 60;
 
-    [Range(1f, 10f)]
+    [SerializeField][Range(1f, 10f)]
     private float _timeOfTheFlight = 5;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
 
     public void ShowTrajectoryLine(Vector3 startpoint, Vector3 startVelocity)
     {
         float timeStep = _timeOfTheFlight / _lineSegments;
 
-        Vector3 []lineRenderPoints = CalculateTrajectoryLine(startpoint, startVelocity, timeStep);
+        Vector3[] lineRenderPoints = CalculateTrajectoryLine(startpoint, startVelocity, timeStep);
         
         lineRenderer.positionCount = _lineSegments;
         lineRenderer.SetPositions(lineRenderPoints);
@@ -31,7 +36,7 @@ public class DrawPotionProjection : MonoBehaviour
 
         for(int i = 1; i < _lineSegments; i++)
         {
-            float timeOffset = timeStep * 1;
+            float timeOffset = timeStep * i;
 
             Vector3 progressBeforeGravity = startVelocity * timeOffset;
             Vector3 gravityOffset = Vector3.up * -0.5f * Physics.gravity.y * timeOffset * timeOffset;
